aea0e0d17618548df875775e5d7a57a7468cf052
ahsana7a9 <a7a9af@gmail.com>
2026-04-07 21:35:20 -0700

    Update usb_monitor.py

--- backend/core/usb_monitor.py
+++ backend/core/usb_monitor.py
@@ -1,60 +1,46 @@
-import os
 import psutil
 import time
-from core.scanner import scan_system
+import os
 
 def get_usb_drives():
     drives = []
-
-    partitions = psutil.disk_partitions()
-
-    for p in partitions:
+    for p in psutil.disk_partitions():
         if 'removable' in p.opts:
             drives.append(p.device)
-
     return drives
 
 
-def monitor_usb():
-
-    print("USB Monitoring Started...")
-
-    known_drives = set(get_usb_drives())
-
-    while True:
-
-        time.sleep(5)
-
-        current_drives = set(get_usb_drives())
-
-        # detect new USB
-        new_drives = current_drives - known_drives
-
-        if new_drives:
-            for drive in new_drives:
-                print(f"USB Inserted: {drive}")
-
-                print("Starting Scan...")
-                results = scan_usb(drive)
-
-                print("Scan Complete:", results)
-
-        known_drives = current_drives
-
-
 def scan_usb(drive_path):
-
     results = []
 
     for root, dirs, files in os.walk(drive_path):
-
         for file in files:
-
-            full_path = os.path.join(root, file)
-
-            results.append(full_path)
+            results.append(os.path.join(root, file))
 
             if len(results) > 50:
                 return results
 
     return results
+
+
+def monitor_usb_with_callback(callback):
+
+    print("USB Monitoring Started...")
+
+    known = set(get_usb_drives())
+
+    while True:
+        time.sleep(5)
+
+        current = set(get_usb_drives())
+        new = current - known
+
+        if new:
+            for drive in new:
+                print(f"USB Inserted: {drive}")
+
+                results = scan_usb(drive)
+
+                callback(results)
+
+        known = current
