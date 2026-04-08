adc5b4dab2cd521bb8d92c0b6b309e4f2bc27712
ahsana7a9 <a7a9af@gmail.com>
2026-04-07 20:54:42 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
-import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <Dashboard />;
+
+  const [result,setResult] = useState([]);
+
+  const runScan = async () => {
+    const res = await axios.get("http://localhost:8000/scan");
+    setResult(res.data.files);
+  }
+
+  return (
+    <div style={{padding:"40px"}}>
+
+      <h1>Hornet Defence</h1>
+
+      <button onClick={runScan}>
+        Start System Scan
+      </button>
+
+      <h2>Scan Results</h2>
+
+      <ul>
+        {result.map((file,i)=>(
+          <li key={i}>{file}</li>
+        ))}
+      </ul>
+
+    </div>
+  );
 }
 
 export default App;
